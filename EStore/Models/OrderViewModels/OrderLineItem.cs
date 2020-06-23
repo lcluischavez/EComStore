e8c17484664273ecd8e7a60283e8a62448cd68fc
lcluischavez <57668579+lcluischavez@users.noreply.github.com>
2020-06-23 16:29:22 -0500

    Order and OrderViewModels: Initial

---
+++ EStore/Models/OrderViewModels/OrderLineItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EStore.Models.OrderViewModels
+{
+    public class OrderLineItem
+    {
+        public Product Product { get; set; }
+    }
+}
